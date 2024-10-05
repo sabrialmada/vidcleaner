3eba1c9b7d2478f5c04cc38f624e6a23abe3d291
Sabrina <sabrialmada1313@gmail.com>
2024-10-05 12:33:02 -0300

    Successful local build, pushing to Vercel

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     presets: ['react-app'],
-    plugins: ['@babel/plugin-transform-private-property-in-object']
+    "plugins": ["@babel/plugin-proposal-private-property-in-object"]
 };
