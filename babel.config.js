119f5275d26bb154d58633ee1d7de78f5af414e9
Sabrina <sabrialmada1313@gmail.com>
2024-10-09 19:33:42 -0300

    babel update

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = {
     plugins: [
         "@babel/plugin-proposal-private-property-in-object",
         "@babel/plugin-transform-private-methods",
-        "@babel/plugin-proposal-optional-chaining"
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-proposal-nullish-coalescing-operator"
     ]
 };
