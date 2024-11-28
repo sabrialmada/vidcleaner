bc50e3d76ce02c3dd8a39017e9f20415e0a1a2e6
Sabrina <sabrialmada1313@gmail.com>
2024-11-28 18:49:26 -0300

    updated front end

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,7 +26,7 @@ const Home = () => {
 
 export default Home; */
 
-import React from 'react';
+/* import React from 'react';
 import Hero from '../components/Hero';
 import Stats from '../components/Stats';
 import Steps from '../components/Steps';
@@ -40,6 +40,36 @@ const Home = () => {
       <div className="hero-section">
         <Hero />
         <Tutorial />
+      </div>
+      <Stats />
+      <Steps />
+      <FAQ /> 
+      <Footer />
+    </div>
+  );
+};
+
+export default Home; */
+
+
+import React from 'react';
+import Hero from '../components/Hero';
+import Stats from '../components/Stats';
+import Steps from '../components/Steps';
+import FAQ from '../components/FAQ';
+import Footer from '../components/Footer'; 
+import Tutorial from '../components/Tutorial/Tutorial';
+
+const Home = () => {
+  return (
+    <div className="home-container">
+      <div className="hero-section">
+        <div className="hero-content">
+          <Hero />
+        </div>
+        <div className="tutorial-content">
+          <Tutorial />
+        </div>
       </div>
       <Stats />
       <Steps />
