313edb87a11d5b7c74a124cfec589726b1f2709c
Sabrina <sabrialmada1313@gmail.com>
2024-11-28 16:57:58 -0300

    updated front end

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -60,7 +60,7 @@ const Tutorial = () => {
             playsInline
             preload="metadata"
           >
-            <source src="/videos/tutorial.mp4" type="video/mp4" />
+            <source src="/tutorial.mp4" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
         </div>
