9a70edb9b09ef66156f0b0aab0b0722171665842
Sabrina <sabrialmada1313@gmail.com>
2024-12-23 22:06:26 -0300

    deleted wrong files

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    presets: ['react-app'],
-    plugins: [
-        "@babel/plugin-proposal-private-property-in-object",
-        "@babel/plugin-transform-private-methods",
-        "@babel/plugin-proposal-optional-chaining",
-        "@babel/plugin-proposal-nullish-coalescing-operator"
-    ]
-};
